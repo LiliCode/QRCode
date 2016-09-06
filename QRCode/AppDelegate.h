9a6d84edabaadba254c05bfa9b12280259b8ef42
Lili <lili2011050508@163.com>
2016-09-06 14:43:45 +0800

    Initial Commit

---
+++ QRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCode
+//
+//  Created by LiliEhuu on 16/9/6.
+//  Copyright © 2016年 LiliEhuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode
+//
+//  Created by LiliEhuu on 16/9/6.
+//  Copyright © 2016年 LiliEhuu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
